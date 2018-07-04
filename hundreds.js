c2732e79aa138aa9cf5c2bd6b0a0e538fe57aee6
StandardGiraffe <dannyfekete@gmail.com>
2018-07-04 02:50:17 +0000

    Updated with some ice cream!

---
+++ hundreds.js
@@ -0,0 +1,9 @@
+function howManyHundreds(number) {
+  return((number / 100)) - ((number % 100) / 100);
+}
+
+// console.log(howManyHundreds(1000), "=?", 10);
+// console.log(howManyHundreds(894), "=?", 8);
+// console.log(howManyHundreds(520), "=?", 5);
+// console.log(howManyHundreds(99), "=?", 0);
+// console.log(howManyHundreds(0), "=?", 0);
---
+++ ice-cream-flavours.js
@@ -0,0 +1,11 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// My code:
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours)
+
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
