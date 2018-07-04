e723e171b7c456ac2b632e53dbfdf2d92de86029
StandardGiraffe <dannyfekete@gmail.com>
2018-07-04 03:24:53 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,12 @@
+var raining = (true);
+var cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {console.log("Short sleeves are fine.")};
+
+console.log("Now you're ready to go outside!");
---
+++ temperature-fun.js
@@ -0,0 +1,7 @@
+var temperature = -5
+
+if (temperature < -40) {
+  console.log("Shit!  Stay inside!");
+} else if (temperature < 0) {
+    console.log("Sounds cold.  Be sure to bundle up!");
+};
